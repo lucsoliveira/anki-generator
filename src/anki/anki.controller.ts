35f8cbf6e865995842122f400e69c711a55cc1ca
Lucas Oliveira <olucas1998@gmail.com>
2024-05-25 14:40:47 -0300

    feat: added loggers

--- src/anki/anki.controller.ts
+++ src/anki/anki.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { AnkiService } from './anki.service';
 import { ResultPhrasesDTO } from './interfaces';
 
 @Controller('anki')
 export class AnkiController {
+  private readonly logger = new Logger(AnkiController.name);
   constructor(private ankiService: AnkiService) {}
   @Post('/phrases/generate')
   async generatePhrases(
@@ -16,12 +17,16 @@ export class AnkiController {
   ): Promise<{ data: ResultPhrasesDTO | null; error?: string }> {
     const { words } = generatePhraseDto.data;
 
+    this.logger.log(`starting generate phrases`);
+    this.logger.debug(`input: ${words.join(',')}`);
+
     try {
       const result = await this.ankiService.generatePhrases(words);
       return {
         data: result,
       };
     } catch (error) {
+      this.logger.error(`${error.message}`);
       return {
         data: null,
         error: error.message,
--- src/chatgpt/chatgpt.service.ts
+++ src/chatgpt/chatgpt.service.ts
@@ -1,13 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import OpenAI from 'openai';
 
 @Injectable()
 export class ChatgptService {
   private client = new OpenAI();
-
+  private readonly logger = new Logger(ChatgptService.name);
   constructor() {}
 
   async start(message: string) {
+    this.logger.verbose(`start -> input: ${message}`);
     const completion = await this.client.chat.completions.create({
       messages: [{ role: 'system', content: message }],
       model: 'gpt-3.5-turbo-0125',
