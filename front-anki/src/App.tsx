7bc27c7c5e1018eb00802efdb987acc35cde861f
Lucas Oliveira <olucas1998@gmail.com>
2024-05-28 13:08:00 -0300

    feat: initial files form front

--- front-anki/src/App.tsx
+++ front-anki/src/App.tsx
@@ -1,35 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [words, setWords] = useState<string[]>([]);
+  const [wordsData, setWordsData] = useState<string>('');
+
+  function handleAddClick(data: string) {
+    console.log({ data });
+    const wordsSplitted = data.split('\n');
+    setWords(wordsSplitted);
+    setWordsData('');
+  }
+
+  function handleRemoveWord(wordIndex: number) {
+    const wordsNew: string[] = JSON.parse(JSON.stringify(words));
+    wordsNew.splice(wordIndex, 1);
+    setWords(wordsNew);
+  }
+
+  function handleCleanWords() {
+    setWords([]);
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <label htmlFor="formGroupExampleInput" className="form-label">
+          <p>Cole aqui as palavras separadas por linhas</p>
+        </label>
+        <textarea
+          value={wordsData}
+          onChange={(event) => {
+            setWordsData(event.currentTarget.value);
+          }}
+          className="form-control"
+          id="formGroupExampleInput"
+          placeholder="Palavras separadas por quebra de linha"
+        />
+        <button
+          onClick={() => {
+            handleAddClick(wordsData);
+          }}
+        >
+          Adicionar
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        <div
+          style={{
+            maxHeight: '300px',
+            overflow: 'auto',
+          }}
+        >
+          <ol className="list-group list-group-numbered">
+            {words.map((val, index) => {
+              return (
+                <li className="list-group-item d-flex justify-content-between align-items-start">
+                  <div className="ms-2 me-auto">
+                    <div className="fw-bold">{val}</div>
+                    {}
+                  </div>
+                  <span
+                    style={{
+                      cursor: 'pointer',
+                    }}
+                    aria-label="Remover palavra"
+                    className="badge text-bg-danger rounded-pill"
+                    onClick={() => {
+                      handleRemoveWord(index);
+                    }}
+                  >
+                    x
+                  </span>
+                </li>
+              );
+            })}
+          </ol>
+        </div>
+
+        <div>
+          <select className="form-select" aria-label="Idioma de entrada">
+            <option selected>Idioma de entrada</option>
+            <option value="3">PT-BR</option>
+            <option value="3">EN</option>
+          </select>
+
+          <select className="form-select" aria-label="Idioma de saída">
+            <option selected>Idioma de saída</option>
+            <option value="3">PT-BR</option>
+            <option value="3">EN</option>
+          </select>
+        </div>
+        <div>
+          {words.length > 0 && (
+            <button onClick={handleCleanWords}>Limpar</button>
+          )}
+          {words.length > 0 && <button>Gerar Frases</button>}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
